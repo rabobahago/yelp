d04d035aa1a6d0a22674b70e5e79095e00d69747
rabobahago <rabobahago@gmail.com>
2021-08-20 15:45:40 +0100

    all endpoints created

--- server/server.js
+++ server/server.js
@@ -15,8 +15,7 @@ app.use((req, res, next) => {
 //get all restaurants
 app.get("/api/v1/restaurants", async (req, res) => {
   try {
-    const { rows } = await db.query("select * from restaurants");
-    console.log(rows);
+    const { rows } = await db.query("SELECT * FROM restaurants");
     res.status(200).json({
       status: "success",
       results: rows.length,
@@ -31,47 +30,77 @@ app.get("/api/v1/restaurants", async (req, res) => {
 
 //get individual restaurant
 
-app.get("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req);
-  res.status(200).json({
-    message: "restaurants successfully created",
-    data: {
-      restaurant: ["mcdonald", "wendy"],
-    },
-  });
+app.get("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const { rows } = await db.query("SELECT * FROM restaurants WHERE id = $1", [
+      req.params.id,
+    ]);
+    res.status(200).json({
+      status: "success",
+      results: rows.length,
+      data: {
+        restaurant: rows,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 //create a restaurant
-app.post("/api/v1/restaurants", (req, res) => {
-  console.log(req);
-  const { name, location, price_range } = req.body;
-  res.status(201).json({
-    message: "restaurants successfully created",
-    data: {
-      name,
-      location,
-      price_range,
-    },
-  });
+app.post("/api/v1/restaurants", async (req, res) => {
+  try {
+    const { rows } = await db.query(
+      "INSERT INTO restaurants (name, location, price_range) VALUES($1, $2, $3) RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    res.status(201).json({
+      status: "new restaurant successfully created",
+      results: rows.length,
+      data: {
+        restaurants: rows[0],
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 //update a restaurant
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
-  res.status(201).json({
-    message: "restaurants successfully created",
-    data: {
-      restaurants: ["wendy"],
-    },
-  });
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const { rows } = await db.query(
+      "UPDATE restaurants SET name=$1, location=$2, price_range=$3 WHERE id=$4 RETURNING *",
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+    res.status(201).json({
+      status: "restaurant updated successfully",
+      results: rows.length,
+      data: {
+        restaurant: rows[0],
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 //delete a restaurant
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params.id);
-  res.status(201).json({
-    message: "restaurants successfully deleted",
-  });
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  try {
+    const { rows } = await db.query("DELETE FROM restaurants WHERE id=$1", [
+      req.params.id,
+    ]);
+    res.status(204).json({
+      status: "successfully delete restaurants",
+      results: rows.length,
+      data: {
+        restaurant: rows[0],
+      },
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 const port = process.env.PORT || 3005;
